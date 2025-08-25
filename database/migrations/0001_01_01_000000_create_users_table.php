1ed3c60d917fcbe6dcf08f8bf413de5735054f44
Sushan Paudyal <mailtosushan@gmail.com>
2025-08-25 17:19:19 +0545

    03. Laravel 11 E-Commerce Project - Admin and User Authentication

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('utype')->default('user')->comment('admin for Admin & user for customer');
             $table->rememberToken();
             $table->timestamps();
         });
