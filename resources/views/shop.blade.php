bc7e0887bc7bd77ef5149c3e58cb6f82cd974276
Sushan Paudyal <mailtosushan@gmail.com>
2025-08-27 17:54:12 +0545

    23. Laravel 11 E-Commerce Project - Product Sorting and Products Per Page

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -323,21 +323,24 @@
                     <div class="breadcrumb mb-0 d-none d-md-block flex-grow-1">
                         <a href="{{ route('home.index') }}" class="menu-link menu-link_us-s text-uppercase fw-medium">Home</a>
                         <span class="breadcrumb-separator menu-link fw-medium ps-1 pe-1">/</span>
-                        <a href={{ route('shop.index') }}" class="menu-link menu-link_us-s text-uppercase fw-medium">The Shop</a>
+                        <a href="{{ route('shop.index') }}" class="menu-link menu-link_us-s text-uppercase fw-medium">The Shop</a>
                     </div>
 
                     <div class="shop-acs d-flex align-items-center justify-content-between justify-content-md-end flex-grow-1">
-                        <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Sort Items"
-                                name="total-number">
-                            <option selected>Default Sorting</option>
-                            <option value="1">Featured</option>
-                            <option value="2">Best selling</option>
-                            <option value="3">Alphabetically, A-Z</option>
-                            <option value="3">Alphabetically, Z-A</option>
-                            <option value="3">Price, low to high</option>
-                            <option value="3">Price, high to low</option>
-                            <option value="3">Date, old to new</option>
-                            <option value="3">Date, new to old</option>
+
+                        <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Page Size" id="pagesize" name="pagesize" style="margin-right: 20px;">
+                            <option value="12" {{ $size == 12 ? 'selected' : ''  }}>Show</option>
+                            <option value="24" {{ $size == 24 ? 'selected' : ''  }}>24</option>
+                            <option value="48" {{ $size == 48 ? 'selected' : ''  }}>48</option>
+                            <option value="102" {{ $size == 102 ? 'selected' : ''  }}>102</option>
+                        </select>
+
+                        <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Sort Items" name="orderby" id="orderby">
+                            <option value="-1" {{ $order == -1 ? 'selected' : '' }}>Default Sorting</option>
+                            <option value="1" {{ $order == 1 ? 'selected' : '' }}>Date (New to Old)</option>
+                            <option value="2" {{ $order == 2 ? 'selected' : '' }}>Date (Old to New)</option>
+                            <option value="3" {{ $order == 3 ? 'selected' : '' }}>Price, (Low to High)</option>
+                            <option value="4" {{ $order == 4 ? 'selected' : '' }}>Price, (High to Low)</option>
                         </select>
 
                         <div class="shop-asc__seprator mx-3 bg-light d-none d-md-block order-md-0"></div>
@@ -455,11 +458,36 @@
 
                 <div class="divider">
                     <div class="flex items-center justify-between flex-wrap gap-10 wgp-pagination">
-                        {{ $products->links('pagination::bootstrap-5') }}
+                        {{ $products->withQueryString()->links('pagination::bootstrap-5') }}
                     </div>
                 </div>
 
             </div>
         </section>
     </main>
+
+
+    <form id="frmFilter" method="GET" action="{{ route('shop.index') }}">
+        @csrf
+        <input type="hidden" name="page" value="{{ $products->currentPage() }}">
+        <input type="hidden" name="size" id="size" value="{{ $size }}">
+        <input type="hidden" name="order" id="order" value="{{ $order }}">
+    </form>
+
+
 @endsection
+
+@push('scripts')
+    <script>
+        $(function (){
+            $("#pagesize").on('change', function (){
+                $('#size').val($("#pagesize option:selected").val());
+                $("#frmFilter").submit();
+            });
+            $("#orderby").on('change', function (){
+                $('#order').val($("#orderby option:selected").val());
+                $("#frmFilter").submit();
+            })
+        })
+    </script>
+@endpush
