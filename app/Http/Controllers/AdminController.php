796fec184f189b4698d35d9b4ccf1d2c7c06a201
Sushan Paudyal <mailtosushan@gmail.com>
2025-08-30 01:33:21 +0545

    41. Laravel 11 E-Commerce Project - Admin Show Order Details

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,9 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Coupon;
 use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\Product;
+use App\Models\Transaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
@@ -465,6 +467,14 @@ class AdminController extends Controller
     {
         $orders = Order::orderBy('created_at', 'desc')->paginate(12);
         return view('admin.orders', compact('orders'));
+    }
+
+    public function order_details($order_id)
+    {
+        $order = Order::find($order_id);
+        $orderItems = OrderItem::where('order_id', $order_id)->orderBy('id')->paginate(12);
+        $transaction = Transaction::where('order_id', $order_id)->first();
+        return view('admin.order-details', compact('order', 'orderItems', 'transaction'));
     }
 
 }
